e4cceade55eeb48a50877b1ff88839c1fb7f8c72
GalacticWafer <mxhjohnson4dba@gmail.com>
2020-09-04 00:27:16 -0600

    removed old markers from previous project

--- keyboards/comfythumbs/keymaps/galacticwafer/keymap.c
+++ keyboards/comfythumbs/keymaps/galacticwafer/keymap.c
@@ -4,13 +4,12 @@
 #include QMK_KEYBOARD_H
 
 extern keymap_config_t keymap_config;
-<<<<<<< Updated upstream
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-=======
+
 //
 //enum layer_names {
 //    _HALMAK,
@@ -21,7 +20,6 @@ extern keymap_config_t keymap_config;
 //
 //#define MY_LSF MO(1)
 //#define MY_RSF MO(2)
->>>>>>> Stashed changes
 
 #define _QWERTY   0
 
