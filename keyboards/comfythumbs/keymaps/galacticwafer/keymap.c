66237e155ecbe075203c20df0ea2ee0910d8e567
GalacticWafer <mxhjohnson4dba@gmail.com>
2020-09-04 00:54:03 -0600

    Initial rebuild

--- keyboards/comfythumbs/keymaps/galacticwafer/keymap.c
+++ keyboards/comfythumbs/keymaps/galacticwafer/keymap.c
@@ -4,63 +4,36 @@
 #include QMK_KEYBOARD_H
 
 extern keymap_config_t keymap_config;
-<<<<<<< Updated upstream
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-=======
-//
-//enum layer_names {
-//    _HALMAK,
-//    _NUMPAD,
-//    _NUMROW_LEFT,
-//    _NUMROW_RIGHT,
-//};
-//
-//#define MY_LSF MO(1)
-//#define MY_RSF MO(2)
->>>>>>> Stashed changes
 
-#define _QWERTY   0
-
-<<<<<<< Updated upstream
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-[_QWERTY] = LAYOUT_ortho_7x7 (
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,    KC_F6,    /**/   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,     KC_MINS,  /**/   KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,     KC_LBRC,  /**/   KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLCK,
-             KC_A,    KC_S,    KC_D,    KC_F,   KC_G,     KC_INS,   /**/   KC_BSLS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-             KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,               /**/            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NLCK,
-    KC_CAPS, KC_APP,  KC_PAUS, KC_LSFT,         KC_BSPC,  KC_LCTL,  /**/   KC_RCTL, KC_SPC,  KC_RSFT,                            KC_ENT,
-    MO(1),  KC_HOME,  KC_PGUP, KC_PGDN, KC_END, KC_LGUI,  KC_LALT,  /**/   KC_RALT, KC_RGUI, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, MO(1)
-),
-
-[1] = LAYOUT_ortho_7x7 (
-    RESET,   _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, RESET,
-    _______, _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, _______,
-             _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, _______,
-             _______, _______, _______, _______, _______,           /**/            _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______,          _______, _______,  /**/   _______, _______, _______,                            _______,
-    _______, _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, _______
-),
+enum layer_names {
+    _HALMAK,
+    _NUMPAD,
+    _NUMROW_LEFT,
+    _NUMROW_RIGHT,
 };
 
-=======
-[0] =  LAYOUT_ortho_7x7 (  
+#define MY_LSF MO(_NUMROW_LEFT)
+#define MY_RSF MO(_NUMROW_RIGHT)
+#define MY_PAD MO(_NUMPAD)
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_HALMAK] =  LAYOUT_ortho_7x7 (  
         KC_ESC,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    /**/   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_DEL,
         KC_GRV,  KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,  /**/   KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_PSCR,
         KC_INS,  KC_W,     KC_L,    KC_R,    KC_B,    KC_Z,    KC_LCBR,  /**/   KC_RCBR, KC_SCLN, KC_Q,    KC_U,    KC_D,    KC_J,     KC_EQL,
         KC_S,    KC_H,     KC_N,    KC_T,    KC_COMM, KC_TAB,            /**/   KC_BSLS, KC_DOT,  KC_A,    KC_E,    KC_O,    KC_I,     KC_QUOT,
         KC_F,    KC_M,     KC_V,    KC_C,    KC_X,                       /**/            KC_G,    KC_P,    KC_SLSH, KC_K,    KC_Y,     KC_APP,
-        KC_CAPS, KC_APP,   KC_PAUS, KC_G,           KC_BSPC, KC_LGUI,  /**/   KC_RGUI, KC_SPC,           KC_H,           KC_ENT,
-        KC_A,   KC_HOME,  KC_PGUP, KC_PGDN, KC_END,  KC_LCTL, KC_LALT,  /**/   KC_RALT, KC_RCTL, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, KC_B
+        KC_CAPS, KC_APP,   KC_PAUS, MY_LSF,             KC_BSPC, KC_LGUI,  /**/    KC_RGUI, KC_SPC,           MY_RSF,           KC_ENT,
+        MY_PAD,   KC_HOME,  KC_PGUP, KC_PGDN, KC_END,  KC_LCTL, KC_LALT,  /**/   KC_RALT, KC_RCTL, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, MY_PAD
 ),
 
-[1] = LAYOUT_ortho_7x7 (
+[_NUMROW_LEFT] = LAYOUT_ortho_7x7 (
         _______, _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, _______,
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,  /**/   KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
         _______, _______, _______, _______, _______, _______, KC_LBRC,  /**/   KC_RBRC, _______, _______, _______, _______, _______, _______,
@@ -70,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, _______
 ),
 
-[2] = LAYOUT_ortho_7x7 (
+[_NUMROW_RIGHT] = LAYOUT_ortho_7x7 (
         _______, _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, _______,
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,  /**/   KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
         _______, _______, _______, _______, _______, _______, KC_LBRC,  /**/   KC_RBRC, _______, _______, _______, _______, _______, _______,
@@ -80,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, _______
 ),
 
-[3] = LAYOUT_ortho_7x7 (
+[_NUMPAD] = LAYOUT_ortho_7x7 (
         RESET,   _______, _______, _______, _______, _______, _______,  /**/   _______, _______, _______, _______, _______, _______, RESET,
         _______, _______, _______, _______, _______, _______, _______,  /**/   _______, _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______,
         _______, _______, _______, _______, _______, _______, _______,  /**/   _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______,
@@ -132,6 +105,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             unregister_code(KC_RSFT);
         }
     }
-    return false;
+    return true;
 }
 
